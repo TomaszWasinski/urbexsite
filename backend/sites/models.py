33fc3b21292cc60f5c635397246431d40a4bdeee
Tomasz Wąsiński <4nt45h@gmail.com>
2016-06-22 21:55:54 +0200

    site model first touch

--- backend/sites/models.py
+++ backend/sites/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Site(models.Model):
+
+    name = models.CharField(max_length=500)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL)
+    # TODO:
+        # discussion
+        # location
+        # province
+        # album
+        # visitors
+        # category
+        # privilages
+        # edit history?
+        # status
+        # value
+        # hardness?
