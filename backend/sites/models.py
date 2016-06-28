ffce17bd3c9e24d7b328641b73474e03ac8020a6
Tomasz Wąsiński <4nt45h@gmail.com>
2016-06-28 22:41:36 +0200

    additional fields

--- backend/sites/models.py
+++ backend/sites/models.py
@@ -12,15 +12,32 @@ class Site(models.Model):
     updated_by = models.ForeignKey(settings.AUTH_USER_MODEL)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
-
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=ACTIVE)
+    score = models.PositiveSmallIntegerField()
+    category = models.ManyToManyField(Categories)
     # TODO:
         # discussion
         # province
         # album
-        # visitors OR visits
-        # category
+        # visitors OR visits (reverse)
         # privilages
         # edit history?
-        # status
-        # value
         # hardness?
+
+    ACTIVE = 'active'
+    INACTIVE = 'inactive'
+    ABANDONED = 'abandoned'
+    STATUS_CHOICES = (
+        (ACTIVE, 'Active'),
+        (INACTIVE, 'Inactive'),
+        (ABANDONED, 'Abandoned')
+    )
+
+    def compute_score(self):
+        pass # TODO
+
+
+class Categories(models.Model):
+
+    name = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
