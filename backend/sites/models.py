52a2a16ae3639e50b6deedf21a29e4c70840f145
Tomasz Wąsiński <4nt45h@gmail.com>
2016-06-29 21:08:31 +0200

    small changes in the model

--- backend/sites/models.py
+++ backend/sites/models.py
@@ -5,24 +5,26 @@ from django.conf import settings
 class Site(models.Model):
 
     name = models.CharField(max_length=500)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL)
     updated_by = models.ForeignKey(settings.AUTH_USER_MODEL)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=ACTIVE)
-    score = models.PositiveSmallIntegerField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=ABANDONED)
+    score = models.PositiveSmallIntegerField(null=True)
     category = models.ManyToManyField(Categories)
     # TODO:
         # discussion
         # province
         # album
         # visitors OR visits (reverse)
-        # privilages
+        # privilages ??
         # edit history?
         # hardness?
+    # more TODO:
+    # privilages system
 
     ACTIVE = 'active'
     INACTIVE = 'inactive'
@@ -34,7 +36,7 @@ class Site(models.Model):
     )
 
     def compute_score(self):
-        pass # TODO
+        pass # TODO after mergeing visits app
 
 
 class Categories(models.Model):
