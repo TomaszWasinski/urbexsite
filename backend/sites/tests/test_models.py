56839157d218d262217fe4fa61ee07049cfaf146
Tomasz Wąsiński <4nt45h@gmail.com>
2016-10-21 23:40:15 +0200

    use pytests fixtures for db setup/teardown

--- backend/sites/tests/test_models.py
+++ backend/sites/tests/test_models.py
@@ -1,19 +1,23 @@
-import mongoengine as me
-from random import random
+import pytest
+from utils.fixtures import mongomock
 
 from ..models import Site
 
 
+@pytest.mark.usefixtures('mongomock')
 class SiteTests:
-
-    def setup_method(self):
-        self.db = me.connect('mongoenginetest', host='mongodb://localhost')
-
-    def teardown_method(self):
-        self.db.drop_database('mongoenginetest')
-        self.db.close()
 
     def test_object_creation(self):
         site = Site(name='test_site')
+        site.save()
+        assert Site.objects.first().name == site.name
+
+    def test_object_creation2(self):
+        site = Site(name='test_site2')
+        site.save()
+        assert Site.objects.first().name == site.name
+
+    def test_object_creation3(self):
+        site = Site(name='test_site3')
         site.save()
         assert Site.objects.first().name == site.name
